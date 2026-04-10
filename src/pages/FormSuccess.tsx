7b1aee82a0972dd4461c5da7ad68463e82a8587d
Rowel <razonabler23@gmail.com>
2026-04-10 21:51:55 +0800

    remove unused component

--- src/pages/FormSuccess.tsx
+++ src/pages/FormSuccess.tsx
@@ -1,5 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 
 function FormSuccess() {
     const navigate = useNavigate();

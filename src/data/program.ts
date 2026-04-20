d9cca598765365b901e82b9246f7ff5f3f394ae2
Rowel <razonabler23@gmail.com>
2026-04-20 19:34:53 +0800

    added program options for shs

--- src/data/program.ts
+++ src/data/program.ts
@@ -16,51 +16,67 @@ export interface CustomSelectOption {
 }
 
 export const PROGRAM_OPTIONS = [
-    { value: "ABA", label: "ABA - Associate in Business Administration" },
-    { value: "ABCOMM", label: "ABCOMM - Bachelor of Arts in Communication" },
-    { value: "AB_ENGLISH", label: "AB ENGLISH - Bachelor of Arts in English" },
-    { value: "ACT", label: "ACT - Associate in Computer Technology" },
-    { value: "BECEd", label: "BECEd - Bachelor in Early Childhood Education" },
-    { value: "BElemEd", label: "BElemEd - Bachelor of Elementary Education" },
-    { value: "BPA", label: "BPA - Bachelor of Sciences in Public Administration" },
-    { value: "BSA", label: "BSA - Bachelor of Science in Accountancy" },
-    { value: "BSAIS", label: "BSAIS - Bachelor of Science in Accounting Information System" },
-    { value: "BSBA_BUS.ECO", label: "BSBA BUS.ECO - B.S. Business Economics" },
-    { value: "BSBA_FINAMGMT", label: "BSBA FINAMGMT - Financial Management" },
-    { value: "BSBA_HRMGMT", label: "BSBA HRMGMT - Human Resource Management" },
-    { value: "BSBA_MKTGMGMT", label: "BSBA MKTGMGMT - Marketing Management" },
-    { value: "BSBA_OPMGMT", label: "BSBA OPMGMT - Operations Management" },
-    { value: "BSC", label: "BSC - Bachelor of Sciences in Criminology" },
-    { value: "BSCOE", label: "BSCOE - Bachelor of Science in Computer Engineering" },
-    { value: "BSCS", label: "BSCS - Bachelor of Science in Computer Science" },
-    { value: "BSED_ENGLISH", label: "BSED ENGLISH - Bachelor in Secondary Education" },
-    { value: "BSED_FILIPINO", label: "BSED FILIPINO - Bachelor in Secondary Education" },
-    { value: "BSED_MATH", label: "BSED MATH - Bachelor in Secondary Education" },
-    { value: "BSED_SCIENCE", label: "BSED SCIENCE - Bachelor in Secondary Education" },
-    { value: "BSELE", label: "BSELE - Bachelor of Science in Electronics Engineering" },
-    { value: "BSHM", label: "BSHM - Bachelor of Science in Hospitality Management" },
-    { value: "BSIA", label: "BSIA - Bachelor of Science in Internal Auditing" },
-    { value: "BSIS", label: "BSIS - Bachelor of Science in Information System" },
-    { value: "BSISM", label: "BSISM - Bachelor of Science in Industrial Security Management" },
-    { value: "BSIT", label: "BSIT - Bachelor of Science in Information Technology" },
-    { value: "BSLM-GBM", label: "BSLM-GBM - Legal Management (General Business)" },
-    { value: "BSLM-LAW", label: "BSLM-LAW - Legal Management (Law)" },
-    { value: "BSM", label: "BSM - Bachelor of Science in Mathematics" },
-    { value: "BSMA", label: "BSMA - Bachelor of Science in Management Accounting" },
-    { value: "BSP", label: "BSP - Bachelor of Sciences in Psychology" },
-    { value: "BSREM", label: "BSREM - Bachelor of Science in Real Estate Management" },
-    { value: "BSTM", label: "BSTM - Bachelor of Science in Tourism Management" },
-    { value: "BTVTEd", label: "BTVTEd - Bachelor in Tech. Voc. Teacher Education" },
-    { value: "DHRM1", label: "DHRM 1 - Diploma in Hotel & Restaurant Management" },
-    { value: "DHRM2", label: "DHRM 2 - Diploma in Hotel & Restaurant Management" },
-    { value: "DHWA", label: "DHWA - Diploma in Health & Wellness Associate" },
+  { value: "ABA", label: "ABA - Associate in Business Administration" },
+  { value: "ABCOMM", label: "ABCOMM - Bachelor of Arts in Communication" },
+  { value: "AB_ENGLISH", label: "AB ENGLISH - Bachelor of Arts in English" },
+  { value: "ACT", label: "ACT - Associate in Computer Technology" },
+  { value: "BECEd", label: "BECEd - Bachelor in Early Childhood Education" },
+  { value: "BElemEd", label: "BElemEd - Bachelor of Elementary Education" },
+  { value: "BPA", label: "BPA - Bachelor of Sciences in Public Administration" },
+  { value: "BSA", label: "BSA - Bachelor of Science in Accountancy" },
+  { value: "BSAIS", label: "BSAIS - Bachelor of Science in Accounting Information System" },
+  { value: "BSBA_BUS.ECO", label: "BSBA BUS.ECO - B.S. Business Economics" },
+  { value: "BSBA_FINAMGMT", label: "BSBA FINAMGMT - Financial Management" },
+  { value: "BSBA_HRMGMT", label: "BSBA HRMGMT - Human Resource Management" },
+  { value: "BSBA_MKTGMGMT", label: "BSBA MKTGMGMT - Marketing Management" },
+  { value: "BSBA_OPMGMT", label: "BSBA OPMGMT - Operations Management" },
+  { value: "BSC", label: "BSC - Bachelor of Sciences in Criminology" },
+  { value: "BSCOE", label: "BSCOE - Bachelor of Science in Computer Engineering" },
+  { value: "BSCS", label: "BSCS - Bachelor of Science in Computer Science" },
+  { value: "BSED_ENGLISH", label: "BSED ENGLISH - Bachelor in Secondary Education" },
+  { value: "BSED_FILIPINO", label: "BSED FILIPINO - Bachelor in Secondary Education" },
+  { value: "BSED_MATH", label: "BSED MATH - Bachelor in Secondary Education" },
+  { value: "BSED_SCIENCE", label: "BSED SCIENCE - Bachelor in Secondary Education" },
+  { value: "BSELE", label: "BSELE - Bachelor of Science in Electronics Engineering" },
+  { value: "BSHM", label: "BSHM - Bachelor of Science in Hospitality Management" },
+  { value: "BSIA", label: "BSIA - Bachelor of Science in Internal Auditing" },
+  { value: "BSIS", label: "BSIS - Bachelor of Science in Information System" },
+  { value: "BSISM", label: "BSISM - Bachelor of Science in Industrial Security Management" },
+  { value: "BSIT", label: "BSIT - Bachelor of Science in Information Technology" },
+  { value: "BSLM-GBM", label: "BSLM-GBM - Legal Management (General Business)" },
+  { value: "BSLM-LAW", label: "BSLM-LAW - Legal Management (Law)" },
+  { value: "BSM", label: "BSM - Bachelor of Science in Mathematics" },
+  { value: "BSMA", label: "BSMA - Bachelor of Science in Management Accounting" },
+  { value: "BSP", label: "BSP - Bachelor of Sciences in Psychology" },
+  { value: "BSREM", label: "BSREM - Bachelor of Science in Real Estate Management" },
+  { value: "BSTM", label: "BSTM - Bachelor of Science in Tourism Management" },
+  { value: "BTVTEd", label: "BTVTEd - Bachelor in Tech. Voc. Teacher Education" },
+  { value: "DHRM1", label: "DHRM 1 - Diploma in Hotel & Restaurant Management" },
+  { value: "DHRM2", label: "DHRM 2 - Diploma in Hotel & Restaurant Management" },
+  { value: "DHWA", label: "DHWA - Diploma in Health & Wellness Associate" },
 
-    // ADD THIS SEPARATOR OBJECT HERE
-     { value: "other", label: "", isHeader: true },
+  // ADD THIS SEPARATOR OBJECT HERE
+  { value: "other", label: "", isHeader: true },
 
-    { value: "BSMedTech", label: "BSMedTech - Bachelor of Sciences in Medical Technology" },
-    { value: "BSN", label: "BSN - Bachelor of Science in Nursing" },
-    { value: "BSRT", label: "BSRT - Bachelor of Science in Radiologic Technology" },
+  { value: "BSMedTech", label: "BSMedTech - Bachelor of Sciences in Medical Technology" },
+  { value: "BSN", label: "BSN - Bachelor of Science in Nursing" },
+  { value: "BSRT", label: "BSRT - Bachelor of Science in Radiologic Technology" },
+
+  // ADD THIS SEPARATOR OBJECT HERE
+  { value: "other", label: "", isHeader: true },
+
+  { value: "ABM", label: "Accountancy, Business and Management (ABM) Strand" },
+  { value: "STEM", label: "Science, Technology, Engineering, and Mathematics (STEM) Strand" },
+  { value: "HUMSS", label: "Humanities and Social Science (HUMSS) Strand" },
+  { value: "GAS", label: "General Academic Strand (GAS)" },
+
+  // ADD THIS SEPARATOR OBJECT HERE
+  { value: "other", label: "", isHeader: true },
+
+  { value: "HE", label: "Home Economics (HE)" },
+  { value: "ICT", label: "Information and Communication Technology (ICT)" },
+  { value: "industrial_arts", label: "Industrial Arts" },
+
 ];
 
 // 2. UPDATE CONCERN_OPTIONS TO USE HEADERS
@@ -81,7 +97,7 @@ export const CONCERN_OPTIONS: CustomSelectOption[] = [
   { value: "h_records", label: "RECORDS & PROFILE", isHeader: true },
   { value: "Enrolled courses missing in ORF", label: "Enrolled courses missing in ORF" },
   { value: "Grade verification", label: "Grade verification" },
-  { value: "Wrong campus selected", label: "Wrong campus selected" },  
+  { value: "Wrong campus selected", label: "Wrong campus selected" },
   { value: "Email Change Request", label: "Email Change Request" },
   { value: "Full Name Correction Request", label: "Full Name Correction Request" },
   { value: "Year Level Correction Request", label: "Year Level Correction Request" },
@@ -97,14 +113,9 @@ export const CONCERN_OPTIONS: CustomSelectOption[] = [
   { value: "Other concern (Click here)", label: "Other concern (Click here)" },
 ];
 
-// On-Time Downpayment with Penalty Issue
-// On-Time Installment with Penalty Issue
-
-// Request for Mode of Payment Change (Full Payment to Installment)
-// Request for Mode of Payment Change (Installment to Full Payment)
-// Email Change Request
-
 export const YEARLEVEL_OPTIONS: Option[] = [
+  { value: "Grade 11", label: "Grade 11" },
+  { value: "Grade 12", label: "Grade 12" },
   { value: "1", label: "1st Year" },
   { value: "2", label: "2nd Year" },
   { value: "3", label: "3rd Year" },
